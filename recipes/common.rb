a4de16e9a802d9dc3b37c9231748b2a1ad34cc2f
lewuathe <lewuathe@me.com>
2015-01-20 20:33:41 +0900

    v0.0.3

--- recipes/common.rb
+++ recipes/common.rb
@@ -18,3 +18,29 @@ directory '/usr/share/storm' do
   mode '0644'
   action :create
 end
+
+cookbook_file "/tmp/#{storm_package_name}.tar.gz" do
+  source "#{storm_package_name}.tar.gz"
+end
+
+script "install_storm" do
+  interpreter "bash"
+  user        "root"
+  code <<-EOL
+    tar zxvf /tmp/#{storm_package_name}.tar.gz -C #{install_dir}
+    rm -rf #{install_dir}/#{storm_version}
+    mv #{install_dir}/#{storm_package_name} #{install_dir}/#{storm_version}
+    chown -R storm:storm #{install_dir}/#{storm_version}
+  EOL
+end
+
+template "#{install_dir}/#{storm_version}/conf/storm.yaml" do
+  source 'storm.yaml.erb'
+  mode '0440'
+  owner 'root'
+  group 'root'
+  variables({
+    :nimbus_ip => node['storm'][:nimbus_ip],
+    :zookeeper_ip => node['storm'][:zookeeper_ip]
+  })
+end
--- recipes/nimbus.rb
+++ recipes/nimbus.rb
@@ -4,32 +4,6 @@ storm_package_name = node['storm'][:package]
 storm_version = node['storm'][:version] 
 install_dir = node['storm'][:install_dir]
 
-cookbook_file "/tmp/#{storm_package_name}.tar.gz" do
-  source "#{storm_package_name}.tar.gz"
-end
-
-script "install_storm" do
-  interpreter "bash"
-  user        "root"
-  code <<-EOL
-    tar zxvf /tmp/#{storm_package_name}.tar.gz -C #{install_dir}
-    rm -rf #{install_dir}/#{storm_version}
-    mv #{install_dir}/#{storm_package_name} #{install_dir}/#{storm_version}
-    chown -R storm:storm #{install_dir}/#{storm_version}
-  EOL
-end
-
-template "#{install_dir}/#{storm_version}/conf/storm.yaml" do
-  source 'storm.yaml.erb'
-  mode '0440'
-  owner 'root'
-  group 'root'
-  variables({
-    :nimbus_ip => node['storm'][:nimbus_ip],
-    :zookeeper_ip => node['storm'][:zookeeper_ip]
-  })
-end
-
 template "/etc/init/storm-nimbus.conf" do
   source 'storm-daemon.conf.erb'
   mode '0644'
