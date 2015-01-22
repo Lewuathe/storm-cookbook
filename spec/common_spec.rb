1da13e3006a1cd3af2e155641c0d8c2eaa78e78a
Bill Warner <bwarner@demark.com>
2015-01-22 10:09:44 -0700

    some cleanup on template rendering and tests to verify

--- spec/common_spec.rb
+++ spec/common_spec.rb
@@ -51,4 +51,9 @@ describe 'storm-cluster::common' do
         group:  'root'
     )
   end
+
+  it 'renders the template storm.yaml tempalte with contents from ./spec/rendered_templates/storm.yaml' do
+    storm_yaml = File.read('./spec/rendered_templates/storm.yaml')
+    expect(chef_run).to render_file('/usr/share/storm/0.9.3/conf/storm.yaml').with_content(storm_yaml)
+  end
 end
