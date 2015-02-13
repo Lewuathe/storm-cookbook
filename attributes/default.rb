11cbdba8968023ce8ae77b7347c98cf2006a9188
Bill Warner <bwarner@demark.com>
2015-02-13 15:27:14 -0700

    reverting change to merge back to upstream master

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,10 @@
 default['storm']['version'] = '0.9.3'
 default['storm']['package'] = "apache-storm-#{node['storm']['version']}"
 default['storm']['install_dir'] = '/usr/share/storm'
-default['storm']['zookeeper_ips'] = []
-default['storm']['nimbus_ip'] = 'nimbus'
-default['storm']['drpc_ips'] = []
+
+default['storm']['zookeeper_ip'] = '192.168.50.3'
+default['storm']['nimbus_ip'] = '192.168.50.4'
+default['storm']['drpc_ip'] = '192.168.50.8'
 
 default['storm']['install_method'] = 'cookbook_file'
 
